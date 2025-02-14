411d77d3fe9ae6d01af4e0d5d89753ecb08046c2
mdero002 <magdalenehenry@gmail.com>
2025-02-14 16:02:37 -0500

    first commit

---
+++ my_script.py
@@ -0,0 +1,11 @@
+#import libraries
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+import plotly.express as px
+import altair as alt
+
+# Load data
+df = pd.read_csv('vehicles_us.csv')
